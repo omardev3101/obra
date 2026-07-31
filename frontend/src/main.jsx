05bdb9ef1067efc7279dbc802bc9f84a6b2dec15
omar3101 <omar.passageminteligente@gmail.com>
2026-07-30 21:20:39 -0300

    Update via deploy script

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/obra/sw.js')
       .then(reg => console.log('Service Worker registrado!', reg))
       .catch(err => console.log('Erro ao registrar Service Worker:', err));
   });
